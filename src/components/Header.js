d9f0efedea819e2658c1a60e5d3e4250b7944802
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-02-28 23:54:48 +0600

    Make the header section responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,11 @@ import { CiCircleChevRight } from "react-icons/ci";
 const Header = () => {
   return (
     <Container>
-      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl pt-4">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl pt-4 text-[#2C384A]">
         Best Website builders in the US
       </h1>
-      <div className="flex items-center justify-between py-3 border-y mt-5 text-sm text-[#4B5665]">
-        <div className="flex items-center gap-6">
+      <div className="flex items-center justify-between flex-col sm:flex-row gap-4 py-3 border-y mt-5 text-sm text-[#4B5665]">
+        <div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-6">
           <div className="flex items-center gap-1">
             <CiCircleChevRight />
             <p>Last Updated - February 22, 2020 </p>
@@ -19,13 +19,15 @@ const Header = () => {
             <p>Advertising Disclosure</p>
           </div>
         </div>
-        <select className="bg-transparent" name="" id="">
-          <option value="Top Relevant">Top Relevant</option>
-          <option value="A - Z">A - Z</option>
-          <option value="Z - A">Z - A</option>
-        </select>
+        <div>
+          <select className="bg-transparent" name="" id="">
+            <option value="Top Relevant">Top Relevant</option>
+            <option value="A - Z">A - Z</option>
+            <option value="Z - A">Z - A</option>
+          </select>
+        </div>
       </div>
-      <div className="space-x-4 flex mt-6 text-sm text-[#4B5665]">
+      <div className="flex gap-4 flex-wrap mt-6 text-sm text-[#4B5665]">
         <span className="bg-white px-5 py-2.5 pr-10 rounded-xl">Tools</span>
         <span className="bg-white px-5 py-2.5 pr-10 rounded-xl">
           AWS Builder
@@ -41,7 +43,7 @@ const Header = () => {
           BlueHosting
         </span>
       </div>
-      <div className="space-x-4 mt-3 text-sm text-[#5C6874]">
+      <div className="hidden sm:block space-x-4 mt-3 text-sm text-[#5C6874]">
         <span>Home</span>
         <span>{">"}</span>
         <span>Hosting for all</span>
