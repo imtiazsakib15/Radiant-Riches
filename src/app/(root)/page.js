e18a837cf7b5c699b8bad77032821afd45c1aab4
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-02-29 16:23:38 +0600

    Sign up section added

--- src/app/(root)/page.js
+++ src/app/(root)/page.js
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import SignUp from "@/components/SignUp";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <SignUp />
     </>
   );
 };
